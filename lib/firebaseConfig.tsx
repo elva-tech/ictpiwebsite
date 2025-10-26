f6710d4015ab581b2c72569a05712a4830d578dd
Yashwanth Reddy s <primeoptimuse154@gmail.com>
2025-10-26 09:04:20 +0530

    fixed firebase

--- lib/firebaseConfig.tsx
+++ lib/firebaseConfig.tsx
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDDlY2LfyZXIbDZIrD1dZHxMtLFLQcKoJA",
-  authDomain: "ictpiwebsite.firebaseapp.com",
-  projectId: "ictpiwebsite",
-  storageBucket: "ictpiwebsite.firebasestorage.app",
-  messagingSenderId: "25459124721",
-  appId: "1:25459124721:web:427d286041a1cf455fbc50",
-  measurementId: "G-S6DDZKK6LH"
+  apiKey: "AIzaSyAV34h82OAX5MaRJtQOOOY_hYmio13Foso",
+  authDomain: "ictpiwebsite-90b76.firebaseapp.com",
+  projectId: "ictpiwebsite-90b76",
+  storageBucket: "ictpiwebsite-90b76.firebasestorage.app",
+  messagingSenderId: "171268728879",
+  appId: "1:171268728879:web:ce2456338b3794019bfeda",
+  measurementId: "G-ELZRQSTEBS"
 };
 
 // Initialize Firebase
